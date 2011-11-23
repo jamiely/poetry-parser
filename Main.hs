da201d0a934fac4388b64de5a72dbcfaf0491296
Jamie Ly <jamie.ly@gmail.com>
2011-11-22 20:09:06 -0500

    Added stub for `classify` fund

--- Main.hs
+++ Main.hs
@@ -1,11 +1,15 @@
 {-# OPTIONS -Wall -Werror -fwarn-tabs -fno-warn-type-defaults -fno-warn-orphans -fno-warn-unused-do-bind #-}
 
+module Main (main) where
+
+import PoemClassifier
+
 main :: IO ()
 main = do
   -- Read the poem in from STDIN
   poem <- getContents
   -- echo the poem and its type
   putStrLn $ "Poem: \n" ++ poem
-  putStrLn $ "Type: Rhyming poem aa"
+  putStrLn $ "Type: " ++ classify poem
 
 
---
+++ PoemClassifier.hs
@@ -0,0 +1,7 @@
+module PoemClassifier (classify) where
+
+-- | Use to classify a poem. Given a poem, this will output information
+-- about its type, rhyming scheme, and meter.
+classify :: String -> String
+classify _ = "Rhyming poem aa"
+
