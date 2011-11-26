ee964fb07795534f00b9df7c8edbc4e4eeb44774
Jamie Ly <jamie.ly@gmail.com>
2011-11-26 16:55:56 -0500

    Separated analyze routines into PoemParser

--- PoemClassifier.hs
+++ PoemClassifier.hs
@@ -1,14 +1,11 @@
 module PoemClassifier (
-  classify, getWords, rhymes, 
-  PoemClassifier.test, Dictionary) where
+  classify, 
+  PoemClassifier.test) where
 
 import CMUPronouncingDictionary
 import PoemParser
 import Test.HUnit
-import Data.Map (Map)
-import qualified Data.Map as Map
 import Data.List.Split(splitOn)
-import Data.Maybe
 
 -- | Use to classify a poem. Given a poem, and dictionary, this will output information
 -- about its type, rhyming scheme, and meter.
@@ -17,6 +14,12 @@ classify poem dictText = poemDescription where
   dict = loadWords dictText $ wordList poem
   words = justWords $ getWords (lines poem) dict
   poemDescription = analyze words
+
+testClassify :: Test 
+testClassify = "Test classify" ~: TestList [
+  -- fails right now
+  classify "something something fox\nsomething something pox" "SOMETHING  S AH1 M TH IH0 NG\nFOX  F AA1 K S\nPOX  P AA1 K S" ~?= "Rhyming poem aa"
+  ]
 
 -- | Returns a description of the poem
 analyze :: [[Word]] -> String
@@ -30,57 +33,6 @@ testAnalyze = "Test analyze" ~: TestList [
 -- | Given a String, splits string on spaces, removing empty strings
 wordList :: String -> [String]
 wordList text = filter (/= "") $ splitOn " " text
-
-testClassify :: Test 
-testClassify = "Test classify" ~: TestList [
-  -- fails right now
-  classify "something something fox\nsomething something pox" "SOMETHING  S AH1 M TH IH0 NG\nFOX  F AA1 K S\nPOX  P AA1 K S" ~?= "Rhyming poem aa"
-  ]
-
-testDictionary :: Dictionary
-testDictionary = Map.fromList [
-  ("fox", testWordFox), 
-  ("pox", testWordPox)
-  ]
-
--- | Given a list of tokens, returns an analysis of the poem pieces
-getWords :: [String] -> Dictionary -> [[Maybe Word]]
-getWords strs dict = map (map (getWord dict)) broken where
-  broken = splitOn ["\n"] strs
-
-testGetWords :: Test
-testGetWords = "Test getWords" ~: TestList [
-  "fox" ~: getWords ["fox"] testDictionary ~?= [[justFox]],
-  "fox with breaks" ~: getWords ["fox", "\n", "fox", "fox"] testDictionary ~?= 
-    [[justFox], [justFox, justFox]]
-  ] where
-  justFox = Just testWordFox
-
--- | Removes all Nothing's from the list and collapses entries
-justWords :: [[Maybe Word]] -> [[Word]]
-justWords w = filter (not . null) (map onlyWords w) where
-  onlyWords :: [Maybe Word] -> [Word]
-  onlyWords maybeWords = map fromJust (filter isJust maybeWords)
-
-testJustWords :: Test
-testJustWords = "Test justWords" ~: TestList [
-  justWords [[Just testWordFox, Nothing], [Nothing, Nothing]] ~?= 
-    [[testWordFox]]
-  ]
-
--- | Returns statistics about a word given the word
-getWord :: Dictionary -> String -> Maybe Word
-getWord dict str = Map.lookup str dict
-
-testGetWord :: Test
-testGetWord = "Test getWord" ~: TestList [
-  "just" ~: getWord testDictionary "fox" ~?= Just testWordFox,
-  "nothing" ~: getWord testDictionary "nothing" ~?= Nothing
-  ]
-
-testWords :: [Word]
-testWords = [testWordFox, testWordPox]
-
 
 -- | Use to determine if two words rhyme. Compares the last 3 phonemes
 -- of each word to determine if they rhyme.
@@ -100,9 +52,6 @@ test :: IO ()
 test = do
   runTestTT (TestList [
     testRhymes,
-    testGetWord,
-    testJustWords,
-    testGetWords,
     testAnalyze,
     testClassify
     ])
--- PoemParser.hs
+++ PoemParser.hs
@@ -1,4 +1,13 @@
-module PoemParser(Token (TokWord, TokNewline), PoemParser.lex) where
+module PoemParser(
+  Token (TokWord, TokNewline), PoemParser.lex,
+  getWords, justWords) where
+
+import CMUPronouncingDictionary
+import Test.HUnit
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.List.Split(splitOn)
+import Data.Maybe
 
 data Token =
     TokWord String -- a word
@@ -11,4 +20,56 @@ lex _ = [something, something, TokWord "fox", TokNewline,
          something, something, TokWord "pox"] where
   something = TokWord "something"
 
+-- | Given a list of tokens, returns an analysis of the poem pieces
+getWords :: [String] -> Dictionary -> [[Maybe Word]]
+getWords strs dict = map (map (getWord dict)) broken where
+  broken = splitOn ["\n"] strs
 
+testGetWords :: Test
+testGetWords = "Test getWords" ~: TestList [
+  "fox" ~: getWords ["fox"] testDictionary ~?= [[justFox]],
+  "fox with breaks" ~: getWords ["fox", "\n", "fox", "fox"] testDictionary ~?= 
+    [[justFox], [justFox, justFox]]
+  ] where
+  justFox = Just testWordFox
+
+testDictionary :: Dictionary
+testDictionary = Map.fromList [
+  ("fox", testWordFox), 
+  ("pox", testWordPox)
+  ]
+
+-- | Removes all Nothing's from the list and collapses entries
+justWords :: [[Maybe Word]] -> [[Word]]
+justWords w = filter (not . null) (map onlyWords w) where
+  onlyWords :: [Maybe Word] -> [Word]
+  onlyWords maybeWords = map fromJust (filter isJust maybeWords)
+
+testJustWords :: Test
+testJustWords = "Test justWords" ~: TestList [
+  justWords [[Just testWordFox, Nothing], [Nothing, Nothing]] ~?= 
+    [[testWordFox]]
+  ]
+
+-- | Returns statistics about a word given the word
+getWord :: Dictionary -> String -> Maybe Word
+getWord dict str = Map.lookup str dict
+
+testGetWord :: Test
+testGetWord = "Test getWord" ~: TestList [
+  "just" ~: getWord testDictionary "fox" ~?= Just testWordFox,
+  "nothing" ~: getWord testDictionary "nothing" ~?= Nothing
+  ]
+
+testWords :: [Word]
+testWords = [testWordFox, testWordPox]
+
+
+test :: IO ()
+test = do
+  runTestTT (TestList [
+    testGetWord,
+    testJustWords,
+    testGetWords
+    ])
+  return ()
